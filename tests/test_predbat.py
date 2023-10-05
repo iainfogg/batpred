07e4514df331ae1b118c58bea8ac6bd493869363
iainfogg <iain@feedthemouse.com>
2023-10-05 22:33:37 +0000

    Some rubbish messing around :(

--- tests/test_predbat.py
+++ tests/test_predbat.py
@@ -1,5 +1,10 @@
-from apps.predbat import predbat
+from apps.predbat.predbat import PredBat as pb
+import pytest_mock
 
-def testIt():
-    assert predbat.PREDICT_STEP == 3
+# def test_call_notify(mocker):
+    # mocker.patch('pb.call_notify', return_value='yo')
+    # obj = object()
+    # x = pb(obj)
+    # x.call_notify('hi')
+    # apps.predbat.predbat.PredBat.call_service.assert_called_once_with('hi')
 
